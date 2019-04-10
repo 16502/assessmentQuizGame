cc3c1d2b4847d25a3971a36a70cde906a39b2f52
Andre Matson <16502@wc.school.nz>
2019-04-10 12:28:51 +1200

    10/04/2019 12:28pm Commit: Adding comments

--- classes/buttons.lua
+++ classes/buttons.lua
@@ -4,12 +4,12 @@ local widget = require("widget")
 
 function buttons:__init(group, xPosition, yPosition, text, width, length, id)
     self.group = group
-    self.xPosition = xPosition
-    self.yPosition = yPosition
-    self.text = text
-    self.width = width
-    self.length = length
-    self.id = id
+    self.xPosition = xPosition -- Assigns the xPosition of the button to the parameter xPosition
+    self.yPosition = yPosition -- Assigns the yPosition of the button to the parameter yPosition
+    self.text = text -- Assigns the text in the button to the parameter text
+    self.width = width -- Assigns the width of the button to the parameter width
+    self.length = length -- Assigns the length of the button to the parameter length
+    self.id = id -- Assigns the id of the button to id
 end
 
 function buttons:buttonRect()
--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 require("classes.30logglobal")
-local buttons = require("classes.buttons")
+local buttons = require("classes.buttons") -- assigns the variable "buttons" to the class buttons so it can be called apon
 -------------------------
 -- Constants
 -------------------------
-local fullWidth = display.contentWeight -- Commenting
+local fullWidth = display.contentWeight
 local fullHeight = display.contentHeight
 local centerX = display.contentCenterX
 local centerY = display.contentCenterY
