49eeb642c7e54aedd188e5b24b65fe9169f568bc
Andre Matson <16502@wc.school.nz>
2019-04-30 09:59:25 +1200

    PUSHHHHHHHHHHHHH

--- classes/buttons.lua
+++ classes/buttons.lua
@@ -2,7 +2,7 @@ local Buttons = class()
 Buttons.__name = "Buttons"
 local widget = require("widget")
 
-function Buttons:__init(group, xPosition, yPosition, text, width, length, id)
+function Buttons:__init(xPosition, yPosition, text, width, length) -- add ,id to the end
     self.group = group
     self.xPosition = xPosition -- Assigns the xPosition of the button to the parameter xPosition
     self.yPosition = yPosition -- Assigns the yPosition of the button to the parameter yPosition
@@ -10,40 +10,26 @@ function Buttons:__init(group, xPosition, yPosition, text, width, length, id)
     self.width = width -- Assigns the width of the button to the parameter width
     self.length = length -- Assigns the length of the button to the parameter length
     self.id = id -- Assigns the id of the button to id
-
-self:draw()
+    self:draw()
 end
 
-function Buttons:drawButtons(text, xPosition, yPosition)
-
-
-
-local widget = require( "widget" )
--- Function to handle button events
-local function handleButtonEvent( event )
-
-    if ( "ended" == event.phase ) then
-        print( "Button was pressed and released" )
+function Buttons:drawButtons()
+    local function handleButtonEvent(event)
+    local buttons = widget.newButton(
+        {
+            label = text,
+            onEvent = handleButtonEvent,
+            emboss = false,
+            -- Properties for a rounded rectangle button
+            shape = "roundedRect",
+            width = width,
+            height = height,
+            cornerRadius = 45,
+            fillColor = { default={1,0,0,1}, over={1,0.1,0.7,0.4} },
+            strokeColor = { default={1,0.4,0,1}, over={0.8,0.8,1,1} },
+            strokeWidth = 4,
+            fontSize = "65"
+        }
+    )
     end
-end
-
--- Create the widget
-local buttons = widget.newButton(
-    {
-        label = text,
-        onEvent = handleButtonEvent,
-        emboss = false,
-        -- Properties for a rounded rectangle button
-        shape = "roundedRect",
-        width = 650,
-        height = 150,
-        cornerRadius = 45,
-        fillColor = { default={1,0,0,1}, over={1,0.1,0.7,0.4} },
-        strokeColor = { default={1,0.4,0,1}, over={0.8,0.8,1,1} },
-        strokeWidth = 4,
-        fontSize = "65"--,
-    }
-)
-end
-
 return Buttons
--- main.lua
+++ main.lua
@@ -17,6 +17,4 @@ print(questions[1])
 print(answers[1])
 print(correctAnswer[1])
 
-Buttons:drawButtons("2", display.contentCenterX, display.contentCenterY)
-Buttons:drawButtons("1", display.contentCenterX, display.contentCenterY + 500)
-Buttons:drawButtons("3", display.contentCenterX, display.contentCenterY - 500)
+Buttons:drawButtons(display.contentCenterX, display.contentCenterY, "1", 600, 600)
